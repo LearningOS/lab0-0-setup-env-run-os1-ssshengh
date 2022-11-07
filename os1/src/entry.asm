40a7b4083dd377f0d7c093a9fde9a4eb4d53cef0
shenheng <ss5205201001@outlook.com>
2022-11-07 10:52:30 +0800

    fix: 增加一些注释

--- os1/src/entry.asm
+++ os1/src/entry.asm
@@ -1,9 +1,12 @@
+    # 放在该段，对应链接文件就可以看见放在了 text 段的第一个位置
     .section .text.entry
+    # 0x80200000
     .globl _start
 _start:
     la sp, boot_stack_top
     call rust_main
 
+    # 段大小是 16 个 1K, 也即 10000(16x)
     .section .bss.stack
     .globl boot_stack
 boot_stack:
